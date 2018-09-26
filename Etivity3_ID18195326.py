6371512acf1c68f19de32288627653a588a854ea
Mark Murnane <MMURNANE@ie.ibm.com>
2018-09-26 17:15:42 +0100

    Initial matrix multiplication

--- Etivity3_ID18195326.py
+++ Etivity3_ID18195326.py
@@ -18,6 +18,9 @@ class Matrix(object):
     
     """
     
+    _ROWS = 0
+    _COLS = 1
+    
     def __init__ (self, element_data):
         """Creates a new instance of a Matrix.
         
@@ -53,7 +56,7 @@ class Matrix(object):
     
 
 
-    def is_valid_add_sub_eq_matrix(self, other):
+    def _is_valid_add_sub_eq_matrix(self, other):
         """Internal helper function that checks if this matrix and other are valid for equality, addition or subtraction.
         
         To be valid for these operations both must be matrices with the same order.
@@ -67,14 +70,25 @@ class Matrix(object):
         
         return True
     
-    
-    def __eq__(self, other):
-        """Compares this matrix object to another matrix object.  Returns true if the order of the matrix is the same and the elements at each position are the same."""
+    def _is_valid_matrix_multiplier(self, other):
+        """Internal helper function that checks if this matrix and other can be validly multiplied.  
+        
+        To be valid the number of columns in this matrix must equal the number of rows in other.
+        """
         
         if not isinstance(other, self.__class__):
             return False
         
-        if self.get_order() == other.get_order():
+        if self.order[Matrix._COLS] != other.order[Matrix._ROWS]:
+            return False
+
+        return True
+    
+    
+    def __eq__(self, other):
+        """Compares this matrix object to another matrix object.  Returns true if the order of the matrix is the same and the elements at each position are the same."""
+        
+        if self._is_valid_add_sub_eq_matrix(other):
             for self_row, other_row in zip(self.elements, other.elements):
                 if self_row != other_row:
                     return False
@@ -84,22 +98,22 @@ class Matrix(object):
     
     
 
-    def __apply_function_to_elements(self, other, lfunc):
+    def _apply_function_to_elements(self, other, lfunc):
         return [list(map(lfunc, self_row, other_row)) for self_row, other_row in zip(self.elements, other.elements) ]
         
         
     def __add__(self, other):
-        add_elements = lambda x, y: x + y
-        new_elements = [list(map(add_elements, self_row, other_row)) for self_row, other_row in zip(self.elements, other.elements) ]
-        
-        return Matrix(new_elements)        
+        if self._is_valid_add_sub_eq_matrix(other):
+            return Matrix (self._apply_function_to_elements(other, lambda x, y: x + y))
+        else:
+            return None        
     
         
     def __sub__(self, other):        
-        sub_elements = lambda x, y: x + y
-        new_elements = [list(map(sub_elements, self_row, other_row)) for self_row, other_row in zip(self.elements, other.elements) ]
-        
-        return Matrix(new_elements)        
+        if self._is_valid_add_sub_eq_matrix(other):
+            return Matrix (self._apply_function_to_elements(other, lambda x, y: x - y))
+        else:
+            return None        
     
             
     def __mul__(self, other):
@@ -111,8 +125,20 @@ class Matrix(object):
             # Scalar multiplication uses list comprehension with a map to multiply each element by the scalar
             new_elements = [list(map(lambda element: element*other, row)) for row in self.elements]
             new_matrix = Matrix(new_elements)
-        elif isinstance(other, self.__class__):
-            pass 
+        elif self._is_valid_matrix_multiplier(other):
+            # Matrix multiplication C=AB, i.e. c = (self)(other)
+            c_order = (self.order[Matrix.__COLS], other.order[Matrix._COLS])
+
+            # Initialise the elements array to an m*n array of 0s
+            c = [[0]*c_order[Matrix._COLS] for i in range(c_order[Matrix._ROWS])]
+            
+            # For each position in the new matrix c, calculate the values from this matrix and other
+            for i in range(c_order[Matrix._ROWS]):
+                for k in range(c_order[Matrix._COLS]):
+                    for j in range(self.order[Matrix._COLS]):
+                        c[i][k] += (self.elements[i][j] * other.elements[j][k])              
+                                    
+            new_matrix = c
             
         return new_matrix
             
@@ -126,8 +152,8 @@ print(b)
 
 c = Matrix([[1, 2, 3], [0, 3, 4], [5, 6, 8]])
 
-print(a != b)
-print(a != c)
+print(a == b)
+print(a == c)
   
 
 d = a + c
@@ -135,3 +161,25 @@ print(d)
 
 e = b - a
 print(e)
+
+
+print ("Matrix multiplication")
+
+f = Matrix([[1, 1], [2, 2]])
+g = Matrix([[2], [1], [3]])
+h = Matrix([[1, 2, 3]])
+i = Matrix([[2, 1], [1, 2], [3, 0]])
+
+print(f)
+print(g)
+print(h)
+print(i)
+
+
+print(a._is_valid_matrix_multiplier(b))
+print(a._is_valid_matrix_multiplier(f))
+print(a._is_valid_matrix_multiplier(g))
+print(a._is_valid_matrix_multiplier(h))
+print(a._is_valid_matrix_multiplier(i))
+
+print(a * g)
